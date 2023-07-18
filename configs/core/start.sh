5ee67d9aaaed94ee0bc66c0cda10e77940f764d7
Leonardo Custodio <leonardo@custodio.me>
2023-07-18 11:04:48 -0300

    Adds platform-ui

--- configs/core/start.sh
+++ configs/core/start.sh
@@ -5,19 +5,19 @@ role=${CONTAINER_ROLE:-app}
 
 if [ "$role" = "ingest" ]; then
     echo "Running ingest..."
-    (cd /var/www/html && php artisan cache:clear && php artisan config:cache && php artisan migrate && php artisan platform:sync && php artisan platform:ingest)
+    (php artisan cache:clear && php artisan config:cache && php artisan migrate && php artisan platform:sync && php artisan platform:ingest)
 elif [ "$role" = "app" ]; then
     echo "Caching configuration..."
-    cd /var/www/html && php artisan cache:clear && php artisan config:cache && php artisan route:cache && php artisan view:cache
+    php artisan platform-ui:install --tenant="no" --skip && php artisan cache:clear && php artisan config:cache && php artisan route:cache && php artisan view:cache
     echo "Running apache..."
     exec apache2-foreground
 elif [ "$role" = "websocket" ]; then
     echo "Running queue and websocket..."
-    cd /var/www/html && php artisan cache:clear && php artisan config:cache
+    php artisan cache:clear && php artisan config:cache
     supervisord -n --configuration /etc/supervisor/supervisord.conf
 elif [ "$role" = "beam" ]; then
     echo "Running beam..."
-    (cd /var/www/html && php artisan cache:clear && php artisan config:cache && php artisan platform:process-beam-claims)
+    (php artisan cache:clear && php artisan config:cache && php artisan platform:process-beam-claims)
 else
     echo "Could not match the container role \"$role\""
     exit 1
