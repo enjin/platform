842f16996f9a479e8ee942a1e33157bae3f30971
Leonardo Custodio <leonardo@enjin.io>
2023-09-06 11:04:55 -0300

    Add logs

--- configs/core/start.sh
+++ configs/core/start.sh
@@ -8,7 +8,7 @@ if [ "$role" = "ingest" ]; then
     (php artisan cache:clear && php artisan config:cache && php artisan migrate && php artisan platform:sync && php artisan platform:ingest)
 elif [ "$role" = "app" ]; then
     echo "Caching configuration..."
-    php artisan platform-ui:install --route="/" --tenant="no" --skip && php artisan cache:clear && php artisan config:cache && php artisan route:cache && php artisan view:cache
+    php artisan log-viewer:publish && php artisan platform-ui:install --route="/" --tenant="no" --skip && php artisan cache:clear && php artisan config:cache && php artisan route:cache && php artisan view:cache
     echo "Running apache..."
     exec apache2-foreground
 elif [ "$role" = "websocket" ]; then
