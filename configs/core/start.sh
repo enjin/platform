53e0fc2400adeeb44855b70f5b2488c9b756d46e
Leonardo Custodio <leonardo@enjin.io>
2023-11-28 23:27:19 -0300

    Revert "[PLA-1446] Add volumes (#26)"
    This reverts commit b02d04aea2500cf5f665a56cca2424566851f508.

--- configs/core/start.sh
+++ configs/core/start.sh
@@ -3,14 +3,12 @@ set -e
 
 role=${CONTAINER_ROLE:-app}
 
-chmod -R 755 storage/logs || true
-chown -hR www-data:www-data storage/logs || true
-
 if [ "$role" = "ingest" ]; then
     echo "Running ingest..."
     (php artisan cache:clear && php artisan config:cache && php artisan migrate && php artisan platform:sync && php artisan platform:ingest)
 elif [ "$role" = "app" ]; then
     echo "Caching configuration..."
+    chown -hR www-data:www-data /var/www/html/storage/logs || true
     APP_KEY=$(awk '$1 ~ /^APP_KEY/' .env | cut -d "=" -f 2)
     if [ -z "$APP_KEY" ]; then
       php artisan key:generate
@@ -20,6 +18,7 @@ elif [ "$role" = "app" ]; then
     exec apache2-foreground
 elif [ "$role" = "websocket" ]; then
     echo "Running queue and websocket..."
+    chown -hR www-data:www-data /var/www/html/storage/logs || true
     php artisan cache:clear && php artisan config:cache
     supervisord -n --configuration /etc/supervisor/supervisord.conf
 elif [ "$role" = "beam" ]; then
